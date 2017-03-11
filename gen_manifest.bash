884960a8865fb7f6eedc6d8ba68a4a572ac67df0
Simao Gomes Viana <xdevs23@outlook.com>
2017-03-11 18:23:17 +0100

    gen_manifest: improve this a little bit
    Change-Id: Ib0eea0b60072ba6a249848721796500a69b88742

--- gen_manifest.bash
+++ gen_manifest.bash
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-function write_xml(){
+function write_xml() {
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
   echo "<ROM>"
-  echo "<RomName>$product</RomName>"
-  echo " <VersionName><![CDATA[ $version ]]></VersionName>"
-  echo "<VersionNumber type=\"integer\">"$(date +%Y%m%d)"</VersionNumber>"
-  echo "<DirectUrl>http://halogenos.org/upload/ROM/7/$device/"$version".zip</DirectUrl>"
-  echo "<HttpUrl>http://halogenos.org/upload/ROM/7/?dir=$device</HttpUrl>"
-  echo "<Android>$android</Android>"
-  echo "<CheckMD5>"$(md5sum $OUT/$version.zip | awk '{print $1}')"</CheckMD5>"
-  echo "<FileSize type=\"integer\">"$(stat --printf="%s" $OUT/$version.zip)"</FileSize>"
-  echo "<Developer>$MAINTAINER</Developer>"
-  echo "<WebsiteURL nil=\"true\">halogenOS.org</WebsiteURL>"
-  echo "<DonateURL nil=\"true\">halogenOS.org/donate</DonateURL>"
+  echo "  <RomName>$product</RomName>"
+  echo "  <VersionName><![CDATA[ $version ]]></VersionName>"
+  echo "  <VersionNumber type=\"integer\">"$(date +%Y%m%d)"</VersionNumber>"
+  echo "  <DirectUrl>https://halogenos.org/upload/ROM/7/$device/${version}.zip</DirectUrl>"
+  echo "  <HttpUrl>https://halogenos.org/upload/ROM/7/?dir=$device</HttpUrl>"
+  echo "  <Android>$android</Android>"
+  echo "  <CheckMD5>"$(md5sum $OUT/$version.zip | awk '{print $1}')"</CheckMD5>"
+  echo "  <FileSize type=\"integer\">"$(stat --printf="%s" $OUT/${version}.zip)"</FileSize>"
+  echo "  <Developer>$MAINTAINER</Developer>"
+  echo "  <WebsiteURL nil=\"true\">halogenOS.org</WebsiteURL>"
+  echo "  <DonateURL nil=\"true\">halogenOS.org/donate</DonateURL>"
   echo "  <Changelog>$CHANGELOG</Changelog>"
   echo "</ROM>"
 }
+
+mkdir -p changelogs
+touch changelogs/${version}.txt
+nano changelogs/${version}.txt
+
+CHANGELOG="$(cat changelogs/${version}.txt)"
+
+if [ -z "$MISTER_MAINTAINER" ]; then
+  echo "Tell me your name, mister maintainer: "
+  read MISTER_MAINTAINER
+  echo "Hi, ${MISTER_MAINTAINER}!"
+fi
+
+MAINTAINER="$MISTER_MAINTAINER"
+
 version="$XOS_VERSION"
 device=$(echo $version | cut -d _ -f 2)
 android=$(echo $version | cut -d _ -f 3)
